33ca7c99126279b5c06dc01f45081cb552017a90
student <you@example.com>
2023-05-30 18:59:50 +0300

    Changes on the style file

--- index.js
+++ index.js
@@ -1,54 +1,27 @@
-function generateProductCards(data) {
+function fetchProducts() {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((collection) => {
+        const returnedProduct = collection.products.slice(3, 9);
+        generateProductCards(returnedProduct);
+      });
+  }
+
+function generateProductCards(collection) {
     const productContainer = document.getElementById("gadgets");
-    data.forEach((product) => {
+    for (const product of collection) {
       const card = document.createElement("div");
       card.classList.add("product-card");
       card.innerHTML = `
-              <img src="${product.thumbnail}" alt="${product.name}" />
-              <h2>${product.title}</h2>
-              <div class='productPrice'>
-              <p>${product.price}</p>
-              <button>ADD TO CART</button>
-              </div>
-          `;
+        <img src="${product.thumbnail}" alt="${product.name}" />
+        <h2>${product.title}</h2>
+        <div class='productPrice'>
+          <p>${product.price}</p>
+          <button>Add to Cart</button>
+        </div>
+      `;
       productContainer.appendChild(card);
-    });
+    }
   }
-  function fetchProducts() {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((data) => {
-        const returnedData = data.products.slice(0, 10);
-        generateProductCards(returnedData);
-    });
-}
-fetchProducts()
-// let availableGadgets = document.getElementById('gadgets')
-// const getGadgets = ()=>{
-//     return fetch('https://dummyjson.com/products')
-//     .then(response => response.json())
-//     .then(response=>response)
-//     .catch(error=>error.message)
-// };
-
-
-// const displayGadgets = async()=>{
-//     const gadgets = await getGadgets();
-//     console.log(gadgets.gadgets);
-//     gadgets.gadgets.map(item =>{
-//         let div = document.createElement('div');
-//         div.className = 'accessories';
-//         let img = document.createElement('img');
-//         let name = document.createElement('h2');
-        
-
-//         img.src = item.images;
-//         name.innerHTML = item.name;
-//         div.appendChild(img);
-//         div.appendChild(name);
-//         availableGadgets.appendChild(div);
-
-//     })
-// }
-// displayGadgets();
-
+  
+  fetchProducts();
